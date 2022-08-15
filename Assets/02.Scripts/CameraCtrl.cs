eb77f98db2658cb30f0803f78764e3d172179388
JCharLee <wotjd999@gmail.com>
2022-08-16 04:06:55 +0900

    캐릭터 추출 및 카메라 구현
    실바나스 캐릭터 추출 및 일부 애니메이션 구현.
    와우 카메라 구현.(진행중)

---
+++ Assets/02.Scripts/CameraCtrl.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    KeyCode leftMouse = KeyCode.Mouse0, rightMouse = KeyCode.Mouse1, middleMouse = KeyCode.Mouse2;
+
+    public float CameraHeight = 1.75f, CameraMaxDistance = 25;
+    float CameraMaxTilt = 90;
+    [Range(0, 4)]
+    public float CameraSpeed = 2;
+    float currentPan, currentTilt = 10, currentDistance = 5;
+    [HideInInspector]
+    public bool autoRunReset = false;
+
+    float panAngle, panOffset;
+    bool camXAdjust, camYAdjust;
+    float rotationXCushion = 3, rotationXSpeed = 0;
+    float yRotMin = 0, yRotMax = 20, rotationYSpeed = 0;
+
+    public CameraState cameraState = CameraState.CameraNone;
+
+    [Range(0.25f, 1.75f)]
+    public float cameraAdjsutSpeed = 1;
+    public CameraMoveState camMoveState = CameraMoveState.OnlyWhileMoving;
+
+    PlayerMove player;
+    public Transform tilt;
+    Camera mainCam;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMove>();
+        player.mainCam = this;
+        mainCam = Camera.main;
+
+        transform.position = player.transform.position + Vector3.up * CameraHeight;
+        transform.rotation = player.transform.rotation;
+
+        tilt.eulerAngles = new Vector3(currentTilt, transform.eulerAngles.y, transform.eulerAngles.z);
+        mainCam.transform.position += tilt.forward * -currentDistance;
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKey(leftMouse) && !Input.GetKey(rightMouse) && !Input.GetKey(middleMouse))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            cameraState = CameraState.CameraNone;
+        }
+        else if (Input.GetKey(leftMouse) && !Input.GetKey(rightMouse) && !Input.GetKey(middleMouse))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            cameraState = CameraState.CameraRotate;
+        }
+        else if (!Input.GetKey(leftMouse) && Input.GetKey(rightMouse) && !Input.GetKey(middleMouse))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            cameraState = CameraState.CameraSteer;
+        }
+        else if ((Input.GetKey(leftMouse) && Input.GetKey(rightMouse)) || Input.GetKey(middleMouse))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            cameraState = CameraState.CameraRun;
+        }
+
+        CameraInputs();
+    }
+
+    private void LateUpdate()
+    {
+        panAngle = Vector3.SignedAngle(transform.forward, player.transform.forward, Vector3.up);
+
+        switch(camMoveState)
+        {
+            case CameraMoveState.OnlyWhileMoving:
+                if (player.moveDir.magnitude > 0 || player.hr != 0)
+                {
+                    CameraXAdjust();
+                    CameraYAdjust();
+                }
+                break;
+
+            case CameraMoveState.OnlyHorizontalWhileMoving:
+                if (player.moveDir.magnitude > 0 || player.hr != 0)
+                    CameraXAdjust();
+                break;
+
+            case CameraMoveState.AlwaysAdjust:
+                CameraXAdjust();
+                CameraYAdjust();
+                break;
+
+            case CameraMoveState.NeverAdjust:
+                CameraNeverAdjust();
+                break;
+        }
+
+        CameraTransforms();
+    }
+
+    void CameraInputs()
+    {
+        if (cameraState != CameraState.CameraNone)
+        {
+            if (!camYAdjust && (camMoveState == CameraMoveState.AlwaysAdjust || camMoveState == CameraMoveState.OnlyWhileMoving))
+                camYAdjust = true;
+
+            if (cameraState == CameraState.CameraRotate)
+            {
+                if (!camXAdjust && camMoveState != CameraMoveState.NeverAdjust)
+                    camXAdjust = true;
+
+                if (player.steer)
+                    player.steer = false;
+
+                currentPan += Input.GetAxis("Mouse X") * CameraSpeed;
+            }
+            else if (cameraState == CameraState.CameraSteer || cameraState == CameraState.CameraRun)
+            {
+                if (!player.steer)
+                {
+                    Vector3 playerReset = player.transform.eulerAngles;
+                    playerReset.y = transform.eulerAngles.y;
+
+                    player.transform.eulerAngles = playerReset;
+
+                    player.steer = true;
+                }
+            }
+
+            currentTilt -= Input.GetAxis("Mouse Y") * CameraSpeed;
+            currentTilt = Mathf.Clamp(currentTilt, -CameraMaxTilt, CameraMaxTilt);
+        }
+        else
+        {
+            if (player.steer)
+                player.steer = false;
+        }
+
+        currentDistance -= Input.GetAxis("Mouse ScrollWheel") * 2;
+        currentDistance = Mathf.Clamp(currentDistance, 0, CameraMaxDistance);
+    }
+
+    void CameraXAdjust()
+    {
+        if (cameraState != CameraState.CameraRotate)
+        {
+            if (camXAdjust)
+            {
+                rotationXSpeed += Time.deltaTime * cameraAdjsutSpeed;
+
+                if (Mathf.Abs(panAngle) > rotationXCushion)
+                    currentPan = Mathf.Lerp(currentPan, currentPan + panAngle, rotationXSpeed);
+                else
+                    camXAdjust = false;
+            }
+            else
+            {
+                if (rotationXSpeed > 0)
+                    rotationXSpeed = 0;
+
+                currentPan = player.transform.eulerAngles.y;
+            }
+        }
+    }
+
+    void CameraYAdjust()
+    {
+        if (cameraState == CameraState.CameraNone)
+        {
+            if (camYAdjust)
+            {
+                rotationYSpeed += (Time.deltaTime / 2) * cameraAdjsutSpeed;
+
+                if (currentTilt >= yRotMax || currentTilt <= yRotMin)
+                    currentTilt = Mathf.Lerp(currentTilt, yRotMax / 2, rotationYSpeed);
+                else if (currentTilt < yRotMax && currentTilt > yRotMin)
+                    camYAdjust = false;
+            }
+            else
+            {
+                if (rotationYSpeed > 0)
+                    rotationYSpeed = 0;
+            }
+        }
+    }
+
+    void CameraNeverAdjust()
+    {
+        switch (cameraState)
+        {
+            case CameraState.CameraSteer:
+            case CameraState.CameraRun:
+                if (panOffset != 0)
+                    panOffset = 0;
+
+                currentPan = player.transform.eulerAngles.y;
+                break;
+
+            case CameraState.CameraNone:
+                currentPan = player.transform.eulerAngles.y - panOffset;
+                break;
+
+            case CameraState.CameraRotate:
+                panOffset = panAngle;
+                break;
+        }
+    }
+
+    void CameraTransforms()
+    {
+        if (cameraState == CameraState.CameraRun)
+        {
+            player.autoRun = true;
+
+            if (!autoRunReset)
+                autoRunReset = true;
+        }
+        else
+        {
+            if (autoRunReset)
+            {
+                player.autoRun = false;
+                autoRunReset = false;
+            }
+        }
+
+        transform.position = player.transform.position + Vector3.up * CameraHeight;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, currentPan, transform.eulerAngles.z);
+        tilt.eulerAngles = new Vector3(currentTilt, tilt.eulerAngles.y, tilt.eulerAngles.z);
+        mainCam.transform.position = transform.position + tilt.forward * -currentDistance;
+    }
+
+    public enum CameraState { CameraNone, CameraRotate, CameraSteer, CameraRun }
+
+    public enum CameraMoveState { OnlyWhileMoving, OnlyHorizontalWhileMoving, AlwaysAdjust, NeverAdjust }
+}
---
+++ Assets/02.Scripts/PlayerMove.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private Transform playerTr;
+    [SerializeField] private Animator anim;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotSpeed = 100f;
+    [SerializeField] private float jumpForce = 5f;
+    private float h, v;
+    [HideInInspector]
+    public Vector3 moveDir;
+    [HideInInspector]
+    public float hr;
+    private bool isJump = false;
+    [HideInInspector]
+    public bool steer;
+    public bool autoRun;
+
+    [HideInInspector]
+    public CameraCtrl mainCam;
+
+
+    private void Awake()
+    {
+        playerTr = GetComponent<Transform>();
+        anim = transform.GetChild(0).GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadMultiply))
+            autoRun = !autoRun;
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        if (v != 0 && !mainCam.autoRunReset)
+            autoRun = false;
+
+        if (autoRun)
+        {
+            v += 1;
+
+            v = Mathf.Clamp(v, -1, 1);
+        }
+
+        if (steer)
+        {
+            h += Input.GetAxis("Horizontal2");
+            h = Mathf.Clamp(h, -1, 1);
+        }
+        if (steer)
+            hr = Input.GetAxis("Mouse X") * mainCam.CameraSpeed * 2f;
+        else
+            hr = Input.GetAxis("Horizontal2");
+
+        moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        playerTr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+        playerTr.Rotate(Vector3.up * hr * rotSpeed * Time.deltaTime);
+
+        if (h != 0 || v != 0)
+            anim.SetBool("IsMove", true);
+        else
+            anim.SetBool("IsMove", false);
+        anim.SetFloat("SpeedX", h);
+        anim.SetFloat("SpeedY", v);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJump)
+            {
+                isJump = true;
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GROUND"))
+            isJump = false;
+    }
+}
